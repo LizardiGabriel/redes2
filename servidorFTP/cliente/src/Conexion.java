65405b62880fca15e0c5edf99fbf02292abe7559
LizardiGabriel <128730421+LizardiGabriel@users.noreply.github.com>
2024-03-13 20:32:26 -0600

    prueba

--- servidorFTP/cliente/src/Conexion.java
+++ servidorFTP/cliente/src/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
     private Socket socket;
     private PrintWriter salida;
     private BufferedReader entrada;
-    String hostName = "192.168.0.155";
+    String hostName = "localhost";
     int portNumber = 1234;
     int portNumber2 = 1235;
     int portNumber3 = 1236;

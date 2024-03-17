3293185c831c1841c041b762b04da787ffc8e111
LizardiGabriel <128730421+LizardiGabriel@users.noreply.github.com>
2024-03-17 14:25:57 -0600

    tajin

--- servidorFTP/cliente/src/Remoto.java
+++ servidorFTP/cliente/src/Remoto.java
@@ -1,14 +1,16 @@
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
+
+
 
 
 
@@ -33,21 +35,21 @@ public class Remoto {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("opcion", "mkdir2");
         jsonObject.put("carpeta", carpeta);
-        String a =  conn.SocketCliente(jsonObject.toString());
+        String a = conn.SocketCliente(jsonObject.toString());
 
     }
 
     public void eliminarAlgo() {
 
-            System.out.print("Escribe el nombre de la carpeta o archivo que quieres eliminar: ");
-            Scanner scanner = new Scanner(System.in);
-            String archivo = scanner.nextLine();
+        System.out.print("Escribe el nombre de la carpeta o archivo que quieres eliminar: ");
+        Scanner scanner = new Scanner(System.in);
+        String archivo = scanner.nextLine();
 
-            Conexion conn = new Conexion();
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.put("opcion", "rmdir3");
-            jsonObject.put("archivo", archivo);
-            String a = conn.SocketCliente(jsonObject.toString());
+        Conexion conn = new Conexion();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("opcion", "rmdir3");
+        jsonObject.put("archivo", archivo);
+        String a = conn.SocketCliente(jsonObject.toString());
     }
 
     public void cambiarDirectorio() {
@@ -71,9 +73,9 @@ public class Remoto {
         Scanner scanner = new Scanner(System.in);
         String archivo = scanner.nextLine();
         String rutita = ruta + "/" + archivo;
-        String rutitaNueva = ruta + "/"+ archivo+".zip";
+        String rutitaNueva = ruta + "/" + archivo + ".zip";
 
-        comprimir(rutita, rutitaNueva);
+        String queEs = comprimir(rutita, rutitaNueva);
 
         File file = new File(rutitaNueva);
         if (!file.exists()) {
@@ -90,6 +92,7 @@ public class Remoto {
         jsonObject.put("opcion", "put5");
         jsonObject.put("nombreOrig", archivo);
         jsonObject.put("tam", tamString);
+        jsonObject.put("type", queEs);
 
         Conexion conn = new Conexion();
         String a = conn.SocketCliente(jsonObject.toString());
@@ -97,32 +100,56 @@ public class Remoto {
         Conexion conn2 = new Conexion();
         conn2.enviarArchivo(rutitaNueva);
 
+
+        // eliminar el .zip
+        try {
+            file.delete();
+        } catch (Exception e) {
+            System.err.println("Error al eliminar el archivo: " + e.getMessage());
+        }
+
+
     }
 
 
-
-    void comprimir(String ruta, String rutaNueva) {
+    String comprimir(String ruta, String rutaNueva)  {
+        String retorno = "";
         try {
-            File sourceFile = new File(ruta);
-            FileOutputStream fos = new FileOutputStream(rutaNueva);
-            ZipOutputStream zipOut = new ZipOutputStream(fos);
-            FileInputStream fis = new FileInputStream(sourceFile);
-            ZipEntry zipEntry = new ZipEntry(sourceFile.getName());
-            zipOut.putNextEntry(zipEntry);
 
-            final byte[] bytes = new byte[1024];
-            int length;
-            while ((length = fis.read(bytes)) >= 0) {
-                zipOut.write(bytes, 0, length);
+            File file = new File(ruta);
+            if (file.isDirectory()) {
+                retorno = "fldr";
+                pack(ruta, rutaNueva);
+
+            }else{
+                retorno = "file";
+
+                File sourceFile = new File(ruta);
+                FileOutputStream fos = new FileOutputStream(rutaNueva);
+                ZipOutputStream zipOut = new ZipOutputStream(fos);
+                FileInputStream fis = new FileInputStream(sourceFile);
+                ZipEntry zipEntry = new ZipEntry(sourceFile.getName());
+                zipOut.putNextEntry(zipEntry);
+
+                final byte[] bytes = new byte[1024];
+                int length;
+                while ((length = fis.read(bytes)) >= 0) {
+                    zipOut.write(bytes, 0, length);
+                }
+
+                fis.close();
+                zipOut.close();
+                fos.close();
+                System.out.println("Archivo comprimido exitosamente.");
+
             }
+        } catch(
+            IOException e)
 
-            fis.close();
-            zipOut.close();
-            fos.close();
-            System.out.println("Archivo comprimido exitosamente.");
-        } catch (IOException e) {
-            System.err.println("Error al comprimir el archivo: " + e.getMessage());
+        {
+           System.err.println("Error al comprimir el archivo: " + e.getMessage());
         }
+        return retorno;
     }
 
     public void recibirArchivo(String localPath) {
@@ -156,6 +183,28 @@ public class Remoto {
 
 
     }
+
+
+    public static void pack(String sourceDirPath, String zipFilePath) throws IOException {
+        Path p = Files.createFile(Paths.get(zipFilePath));
+        try (ZipOutputStream zs = new ZipOutputStream(Files.newOutputStream(p))) {
+            Path pp = Paths.get(sourceDirPath);
+            Files.walk(pp)
+                    .filter(path -> !Files.isDirectory(path))
+                    .forEach(path -> {
+                        ZipEntry zipEntry = new ZipEntry(pp.relativize(path).toString());
+                        try {
+                            zs.putNextEntry(zipEntry);
+                            Files.copy(path, zs);
+                            zs.closeEntry();
+                        } catch (IOException e) {
+                            System.err.println(e);
+                        }
+                    });
+        }
+    }
+
+
 
 
 

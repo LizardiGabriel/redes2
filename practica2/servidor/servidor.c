243c1d557a73c8c97f150adadb19ca80ce7d0d8a
LizardiGabriel <128730421+LizardiGabriel@users.noreply.github.com>
2024-04-10 22:31:41 -0600

    organizacion

---
+++ practica2/servidor/servidor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Socket2.h"
+#include "List.h"
+
+
+int main() {
+    int puerto = 1234;
+    int ejecutar = iniciarSocket(puerto);
+    return 0;
+}
